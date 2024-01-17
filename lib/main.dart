119dae9b7f2590c31531d1b5aeebb7fc58f6dfba
teodor <teodor.mytsyk@gmail.com>
2024-01-17 16:25:26 +0100

    Updated app design colors;

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:test_exercise/constants.dart';
 
 import 'app_router.dart';
 import 'injection.dart' as di;
@@ -13,12 +15,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      title: 'Flutter GoRouter Example',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+        lightColorScheme = ColorScheme.fromSeed(
+          seedColor: brandColor,
+        );
+        darkColorScheme = ColorScheme.fromSeed(
+          seedColor: brandColor,
+          brightness: Brightness.dark,
+        );
+
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          title: 'Flutter GoRouter Example',
+          theme: ThemeData(
+            colorScheme: lightColorScheme,
+            // extensions: [lightCustomColors],
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+            // extensions: [darkCustomColors],
+          ),
+        );
+      },
     );
   }
 }
